1850f92051d88ec9ca9dadcc12696ac49460b90f
Andrea <andrea.bolla@insa-lyon.fr>
2024-12-17 10:17:56 +0100

    Create Constructor from C-String

--- String.cpp
+++ String.cpp
@@ -1,3 +1,17 @@
 #include "String.h"
+#include <iostream> 
+
+//Constructor from c-string 
+String::String(const char* my_c_string) {
+	size = 0; 
+	while (my_c_string[size] != 0) size++; 
+	capacity = size + 1; 
+	data = new char[capacity]
+	for (size_t i = 0; i < size; i++) {
+		data[i] = my_c_string[i]; 
+	}
+	data[size] = '\0'; 
+}
+
 
 
