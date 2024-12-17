10960772b6c864d825cf31e7ffde67c77f470607
Andrea <andrea.bolla@insa-lyon.fr>
2024-12-17 10:42:46 +0100

    Modify Constructor implementation

--- String.cpp
+++ String.cpp
@@ -3,14 +3,14 @@
 
 //Constructor from c-string 
 String::String(const char* my_c_string) {
-	size = 0; 
-	while (my_c_string[size] != 0) size++; 
-	capacity = size + 1; 
-	data = new char[capacity]
-	for (size_t i = 0; i < size; i++) {
-		data[i] = my_c_string[i]; 
+	size_ = 0; 
+	while (my_c_string[size_] != 0) size_++; 
+	capacity_ = size_ + 1; 
+	data_ = new char[capacity_]
+	for (size_t i = 0; i < size_; i++) {
+		data_[i] = my_c_string[i]; 
 	}
-	data[size] = '\0'; 
+	data_[size_] = '\0'; 
 }
 
 
