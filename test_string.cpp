41798eb2d88670527461876226b0d83a98f4eea1
Andrea <andrea.bolla@insa-lyon.fr>
2025-01-02 15:24:49 +0100

    Add a new file to test the class methods

---
+++ test_string.cpp
@@ -0,0 +1,22 @@
+#include "String.h"
+#include <iostream>
+
+
+int main() {
+
+    // TEST OF THE DEFAULT CONSTRUCTOR
+    std::cout << "\n***********************************************************\n";
+    std::cout << "TEST 1: DEFAULT CONSTRUCTOR\n";
+    std::cout << "***********************************************************\n";
+    std::cout << "Press any key to test the default constructor.\n"
+              << "For example, the initialization of two objects with the string 'Hello World' will be demonstrated below.\n";
+    std::cin.get();
+
+    // Create the object
+    Dummy_class obj1;
+
+    // Print the data of the object
+    std::cout << "Object 1 - ID: " << obj1.getId() << ", String: " << obj1.c_str() << ", Size: " << obj1.Size() << std::endl;
+
+    Dummy_class obj2;
+    std::cout << "Object 2 - ID: " << obj2.getId() << ", String: " << obj2.c_str() << ", Size: " << obj2.Size() << std::endl << std::endl;
